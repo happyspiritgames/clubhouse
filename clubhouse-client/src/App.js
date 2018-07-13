b9947315b29d424727dbe5a5254af8ca9b1f3d48
Dave Mount <davemount@gmail.com>
2018-07-13 10:25:00 -0700

    It's a start

--- clubhouse-client/src/App.js
+++ clubhouse-client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
 
 class App extends Component {
   render() {
@@ -10,16 +11,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <a href="https://happyspiritgames.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=3jgcupa4o95gcq98qmimb2qqt7&redirect_uri=http://localhost:3000/signin">Sign in for a code.</a>
-        </p>
-        <p className="App-intro">
-          <a href="https://happyspiritgames.auth.us-west-2.amazoncognito.com/login?response_type=token&client_id=3jgcupa4o95gcq98qmimb2qqt7&redirect_uri=http://localhost:3000/signin">Sign in for a token.</a>
-        </p>
+        <div className="App-intro">
+          <Login />
+        </div>
       </div>
     );
   }
 }
 
-// https://happyspiritgames.auth.us-west-2.amazoncognito.com/oauth2/idpresponse
 export default App;
