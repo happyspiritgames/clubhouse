235b1fb42aae95cc437bb81c6ddf9b9511fee8a3
Dave Mount <davemount@gmail.com>
2018-07-14 16:13:09 -0700

    Got some basic tests running.

---
+++ ts-react/src/components/Hello.test.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as enzyme from 'enzyme';
+import Hello from './Hello';
+
+it('renders when no enthusiasm level is given', () => {
+  const hello = enzyme.shallow(<Hello name='Daniel' />);
+  expect(hello.find(".greeting").text()).toEqual('Hello, Daniel.')
+});
+
+it('renders with enthusiasm greater than 0', () => {
+  const hello = enzyme.shallow(<Hello name='Daniel' enthusiasmLevel={1} />);
+  expect(hello.find(".greeting").text()).toEqual('Hello, Daniel!');
+});
+
+it('throws when enthusiasm is negative', () => {
+  expect(() => {
+    enzyme.shallow(<Hello name='Daniel' enthusiasmLevel={-1} />);
+  }).toThrow();
+})
