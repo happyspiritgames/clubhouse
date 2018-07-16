0782c93c46f6f8f24be150d4f69e05b3da6b21ec
The Happy Spirit <dave@happyspiritgames.com>
2018-07-16 13:58:37 -0700

    Convert page to stateless functional component.

--- ts-react/src/components/MemberHomePage.tsx
+++ ts-react/src/components/MemberHomePage.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 
-function MemberHomePage() {
+export interface MemberHomePageProps { compiler: string; framework: string; }
 
-  return (
-    <div>
-      <h1>Member Home Page</h1>
-      <p>
-        This page is aware of whether the user is signed in.
-      </p>
-    </div>
-  );
-}
-
-export default MemberHomePage;
-
+export const MemberHomePage = (props: MemberHomePageProps) => (
+  <div>
+    <h1>Member Home Page</h1>
+    <p>
+      This page is aware of whether the user is signed in.
+    </p>
+  </div>
+);
